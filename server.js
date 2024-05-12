9cb336222e348d99ed8340158175997a6824430f
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2024-05-12 11:55:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import fetchData from "./index.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/generate", async (req, res) => {
+  const { preferences, budget, numTravelers, month } = req.body;
+
+  try {
+    const data = await fetchData(preferences, budget, numTravelers, month);
+    if (data) {
+      res.send({ data });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error occurred while generating content. Please try again.",
+    });
+  }
+});
+
+app.get('/',(req,res)=>{
+  res.send("Hello")
+})
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
